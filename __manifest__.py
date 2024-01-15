eba66a82af59a8e7c3623d5ffbd7bc2ad0bf2c46
Antonio Garrido Masse <agarmas03@gmail.com>
2024-01-15 14:19:38 +0100

    Item

---
+++ __manifest__.py
@@ -0,0 +1,32 @@
+{
+    'name': "dnd",
+
+    'summary': """DnD 5e assistance""",
+
+    'description': """Manage DnD 5e player game""",
+
+    'author': "Antonio Garrido Massé",
+    'website': "",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Games',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+    ],
+    
+    # only loaded in demonstration mode
+    'demo': [   
+    ],
+    
+    'installable': True,
+    'application': True,
+}
---
+++ models/item.py
@@ -0,0 +1,37 @@
+from odoo import models, fields, api
+
+
+class Item(models.Model):
+    #Special fields
+    _name = 'dnd.machine'
+    _description = 'Posible items in the game'
+
+    #Fields
+    name = fields.Char(
+        string='Name',
+        size=255,
+        required=True,
+        help='Item\'s name'
+    )
+
+    type1 = fields.Char(
+        string='First type', 
+        size=255, 
+        help='Item\'s type1'
+    )
+
+    type2 = fields.Char(
+        string='Second type',
+        size=255,
+        help='Item\'s type2'
+    )
+
+    weight = fields.Integer(
+        string='Weight',
+        help='Item\'s weight (lbs)' 
+    )
+
+    price = fields.Integer(
+        string='Price',
+        help='Item\'s price (PO)'
+    )       
